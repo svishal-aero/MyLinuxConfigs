5f7da72718c27a2142dd4ec05ef2ffb58afccf93
svishal-aero <svishal.aero@gmail.com>
2024-02-21 01:11:41 -0500

    New commit

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -3,3 +3,5 @@ require('packages')
 require('_treesitter')
 require('_lsp')
 require('_telescope')
+
+vim.lsp.set_log_level('off')
--- configs/nvim/lua/_lsp.lua
+++ configs/nvim/lua/_lsp.lua
@@ -16,7 +16,7 @@ lsp.set_preferences({
 })
 
 lsp.setup_nvim_cmp({
-    mapping = cmp_mappings,
+    mapping = cmp_mappings
 })
 
 lsp.on_attach(function(client, bufnr)
@@ -29,7 +29,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set('n', '<leader>o', function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set('n', '<leader>;', function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set('n', '<leader>,', function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
+    --vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
 
 end)
 
